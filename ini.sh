5c194b38d15654a5ca30c366fc8684cb03fadcdd
Atreb92 <36992456+Atreb92@users.noreply.github.com>
2020-04-08 19:09:13 +0200

    added init.sh

---
+++ ini.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#
+# This script has been written under ubuntu 19.10 to download and install
+#
+#
+
+###############################################################################
+# Update our machine to the latest code if we need to.
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo 'This script must be run with root/sudo privileges' >&2
+    exit 1
+fi
+
+apt update && apt upgrade -y
+
+if [ -f /var/run/reboot-required ]; then
+    echo "A reboot is required in order to proceed with the install." >&2
+    echo "Please reboot and re-run this script to finish the install." >&2
+    exit 1
+fi
+
+echo "Which IDE would you like to install?"
+echo "1. Netbeans"
+echo "2. Eclipse"
+echo "3. IntelliJ idea community edition"
+echo ""
+echo -n "Choose 1, 2 or 3: " 
+read ideChoice
+
+while [ $ideChoice != 1 -a $ideChoice != 2 -a $ideChoice != 3 ]
+do
+    echo -n "Invalid input, choose 1, 2 or 3: "
+    read ideChoice
+done
+
+case $ideChoice in
+    1)
+    echo "Netbeans will be installed"
+    apt-get install -y netbeans
+    ;;
+    2)
+    echo "Eclipse will be installed"
+    snap install --classic eclipse
+    ;;
+    3)
+    echo "IntelliJ idea community edition will be installed"
+    snap install --classic intellij-idea-community
+    ;;
+esac
+
+apt-get install -y openjdk-13-jdk nodejs mosquitto mongodb-clients mongodb
+
+snap install node-red
+
+echo "Install is complete."
